d9497237adf78971a02df833679f401aa57e00bf
sebastian espeset <sebastian.c.espeset@gmail.com>
2020-10-27 17:41:20 -0700

    added a random color generator on keydown

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
-console.log("hello world")// Your code goes here
-
+console.log("hello world"); // Your code goes here
 
 //STEP 1, name some stuff....
 
-
 //whole header
-const headerEffect = document.querySelector("header")
+const headerEffect = document.querySelector("header");
 console.log(headerEffect);
 
 //"fun bus"
@@ -29,7 +27,7 @@ const secondContent = document.querySelector(".content-section");
 console.log(secondContent);
 
 //second chunk of img and text
-const thirdContent = document.querySelector("div .inverse-content")
+const thirdContent = document.querySelector("div .inverse-content");
 console.log(thirdContent);
 
 //third chunk of img and text
@@ -37,11 +35,30 @@ const fourthContent = document.querySelector("div .content-destination");
 console.log(fourthContent);
 
 //choosing destination(all p's)
- const chooseDestination  = document.querySelector(".content-pick p");
- console.log(chooseDestination);
+const chooseDestination = document.querySelector(".content-pick p");
+console.log(chooseDestination);
 
- //sign me up redirect
- const signUpRedirect = document.querySelector("footer p");
- console.log(signUpRedirect);
+//sign me up redirect
+const signUpRedirect = document.querySelector("footer p");
+console.log(signUpRedirect);
 
- //All elements I plan on adding effects too are labled and ready to treat.
+//-|-All elements I plan on adding effects too are labled and ready to treat-|-
+
+//click event
+headerEffect.addEventListener("click", function (event) {
+  event.target.style.backgroundColor = "blue";
+});
+
+//disapeparing bus
+funBusEffect.addEventListener("mouseover", function (event) {
+  event.target.style.display = "none";
+});
+
+//keydown makes background color change bar bigger
+
+document.addEventListener("keydown", function (event) {
+  var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  event.target.style.backgroundColor = "randomColor";
+  event.target.style.backgroundColor = `#${randomColor}`;
+  alert(`the color is #${randomColor}`);
+});
